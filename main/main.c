7ffc43e0896c995c5feb259c9b9fb70961d99f90
danilogf91 <danilogrnd@gmail.com>
2023-02-08 23:44:19 -0500

    test first connection ok

--- main/main.c
+++ main/main.c
@@ -1,6 +1,66 @@
-#include <stdio.h>
+#include "wifi_connection.h"
+#include "driver/gpio.h"
 
-void app_main(void)
+#define PIN_INTERRUPT 15
+static const char* TAG = "main program";
+SemaphoreHandle_t ota_semaphore = NULL;
+
+void on_button_pushed (void* params)
 {
+    xSemaphoreGiveFromISR (ota_semaphore, pdFALSE);
+}
+
+
+void run_ota (void* params)
+{
+    while ( true )
+    {
+        xSemaphoreTake (ota_semaphore, portMAX_DELAY);
+        ESP_LOGI (TAG, "Disconnect network");
+        wifi_disconnect ();
+
+    }
+}
+
+void app_main (void)
+{
+    wifi_credentials_t wifi = {
+        .ssid = "hPalmiraCM",
+        .pass = "Hpalmira17"
+    };
+
+    //Initialize semaphore
+    ota_semaphore = xSemaphoreCreateBinary ();
+    //Create task
+    xTaskCreate (run_ota, "run_ota", 1024 * 8, NULL, 2, NULL);
+
+    //Initialize INTERRUP SERVICE
+
+    gpio_config_t gpioConfig = {
+    .pin_bit_mask = 1ULL << PIN_INTERRUPT,
+    .mode = GPIO_MODE_DEF_INPUT,
+    .pull_up_en = GPIO_PULLUP_ENABLE,
+    .pull_down_en = GPIO_PULLUP_DISABLE,
+    .intr_type = GPIO_INTR_POSEDGE };
+    gpio_config (&gpioConfig);
+    gpio_install_isr_service (0);
+    gpio_isr_handler_add (PIN_INTERRUPT, on_button_pushed, ( void* ) PIN_INTERRUPT);
+
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init ();
+    if ( ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND )
+    {
+        ESP_ERROR_CHECK (nvs_flash_erase ());
+        ret = nvs_flash_init ();
+    }
+    ESP_ERROR_CHECK (ret);
+
+    ESP_LOGI (TAG, "ESP_WIFI_MODE_STA");
+
+    //Initialize WIFI STATION
+    wifi_init_sta (wifi);
 
 }
+
+
+
