e19330e9e181cb1caecefe5344bb065c0ddc59c7
danilogf91 <danilogrnd@gmail.com>
2023-02-08 23:19:23 -0500

    wifi connection added

---
+++ components/wifi_connection/include/wifi_connection.h
@@ -0,0 +1,61 @@
+// void test (void);
+#include "stdio.h"
+#include "string.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/semphr.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "esp_random.h"
+#include "math.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+#define EXAMPLE_ESP_WIFI_SSID      "hPalmiraCM"
+#define EXAMPLE_ESP_WIFI_PASS      "Hpalmira17"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  5
+
+#if CONFIG_ESP_WIFI_AUTH_OPEN
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
+#elif CONFIG_ESP_WIFI_AUTH_WEP
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WEP
+#elif CONFIG_ESP_WIFI_AUTH_WPA_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_WPA2_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA3_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA2_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_WPA3_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WAPI_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WAPI_PSK
+#endif
+
+#define WIFI_STATUS_LED 25
+#define ON 1
+#define OFF 0
+
+typedef struct wifi_credentials
+{
+    unsigned char ssid[32];
+    unsigned char pass[64];
+}wifi_credentials_t;
+
+
+void wifi_init_sta (wifi_credentials_t wifi);
+void event_handler (void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
+void wifi_connect (void);
+void wifi_disconnect (void);
+void wifi_status_led (void* pvParameters);
+
